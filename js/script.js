0fa3fe1f6c353622aae9963e883aad839397a665
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-24 18:13:39 +0100

    js send inizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// alert('Ciao')
+$(document).ready(function(){
+  //alert('Ciao')
+  $('.icon_send').click(function(){
+    sendMessage();
+  });
+});
+
+function sendMessage() {
+  var textMessage = $('input .send_message').val();
+  if(textMessage.length != 0) {
+    var newMessage = $('.template .message').clone();
+    console.log(newMessage);
+  }
+}
