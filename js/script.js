83fa5cf3db7afcc20438ba8509f4774f57ed0866
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-25 10:42:43 +0100

    milestone1

--- js/script.js
+++ js/script.js
@@ -6,9 +6,30 @@ $(document).ready(function(){
 });
 
 function sendMessage() {
-  var textMessage = $('input .send_message').val();
-  if(textMessage.length != 0) {
+  var textMessage = $('input.send_message').val();
+  console.log(textMessage );
+  if (textMessage.length != 0) {
     var newMessage = $('.template .message').clone();
     console.log(newMessage);
+
+    newMessage.find('.message_text').text(textMessage);
+
+    var date = new Date();
+    var hours = addZero(date.getHours());
+    var minutes = addZero(date.getMinutes());
+    var time = hours + ':' + minutes;
+    newMessage.find('.message_time').text(time);
+    newMessage.addClass('send');
+    $('.chat_center').append(newMessage);
+    $('input.send_message').val('');
+
   }
+}
+
+
+function addZero(number) {
+  if (number < 10) {
+    number = '0' * number
+  }
+  return number;
 }
